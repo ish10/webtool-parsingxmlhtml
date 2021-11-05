53d74fcd79648ded8c1f5b5547c81d96b47746b9
MarwaMohamed <marwam.2@hotmail.com>
2021-11-05 09:14:29 -0500

    adding more styles and connect it to power portal

--- ConsoleApp10/JS/index.js
+++ ConsoleApp10/JS/index.js
@@ -75,3 +75,5 @@ previousbutton1.addEventListener('click', () => {
 
 
 })
+
+
--- ConsoleApp10/Pojo/data.cs
+++ ConsoleApp10/Pojo/data.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MigrationTool.Pojo
+﻿namespace MigrationTool.Pojo
 {
-    class data
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines the <see cref="data" />.
+    /// </summary>
+    internal class data
     {
+        /// <summary>
+        /// Gets or sets the childtag.
+        /// </summary>
         public Dictionary<string, String> childtag { get; set; }
     }
 }
